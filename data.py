20f7b88e193434247a749d91238245505dcc7d05
MkSeven2 <sarsembayevdias7@gmail.com>
2025-02-06 20:26:59 -0600

    Create data.py

---
+++ data.py
@@ -0,0 +1,38 @@
+# data.py
+
+DATA = {
+    "Example": {
+        "NmNhWaZv2hJVG0r2n8U432fXEg7XeIsyA0Q1zsL0kh826sF08wR9xdGoxyWs2aMNLk9fGAD4RkhV9DFESuEBOshSsm": "DiasDamina",
+        "jPwIhjye7bp6fMA57mnucL5sMIC7BElWZhxlHfui0CqtLk5nnr": "OhNoBabyCryy"
+    },
+    "Product1": {
+        "WQDqqx2j3Fh9oCG7EmFCIhimgxW3AobF1IZpVXsZH4uVYjK8gM": "alia_sarik",
+        "zcfAmGDJ3kFT4FKh5so6UUbkL31U6W9yywlUw60U0UVB1VeyBi": "Robzilain4",
+        "Hcrer8gzRiJQm3P8Pnhfo0kw7m6tCjzfcDYQChuPB5NwvaX25O": "Damil_100",
+        "DJZZBnEa1nLn7TxoRddu8vQA8Qyae6MAkI94ARI7dQyJn5Iq4S": "DiasDamina",
+        "LOXqLVRfXr1TwvlDnETSPMGPnnuRoHKZmXxE0gQW9sV04YN5xw": "OhNoBabyCryy"
+    },
+    "McDonald": {
+        "rnfJsA_Z-.2DFT%hbtO3W=F897xITO4WFymgaE1s": "alia_sarik",
+        "mUs6vW4MD994vNUQW7whMKb7u7hUwPGj": "DiasDamina"
+    },
+    "BEPC Electricity": {
+        "FWR2E9n5iRqUzTlCfRuz6gy4u6jOnKlm": "DiasDamina",
+        "wx546G0rUZa8qwDSsTVz4DCRY8MfzboZ": "OhNoBabyCryy"
+    },
+    "AntiCheat": {
+        "3nJdiZDaEublqS4VjbhUDXxJQ7AYq5glvmfTorrPFXsVYens55": "DiasDamina",
+        "5wjbwdZlcdjt8zfoS73LFmn2lo38XtqqRLvFbvUOFZNlVQES8d": "OhNoBabyCryy"
+    },
+    "k9AfI9TFE202Zw6h2lgxpHHvk": {
+        "b02MjsgbNjLp2emTz1dhRC3xHtb1j7NxF4c": "diasdamina",
+        "admin": "DiasDamina",
+        "admin2": "OhNoBabyCryy"
+    },
+    "AI Bot": {
+        "4092db0f-0dad-4c2e-9da3-5b12408eb0f2": "OhNoBabyCryy"
+    },
+    "Proxy Service": {
+        "07e62944-7023-4c7b-aebf-d788a9c9b5b5": "DiasDamina"
+    }
+}
